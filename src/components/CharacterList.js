cbe2201aa3745dcca72ea593a1d8c748b46e03e0
josepaniagua <jpanjr@gmail.com>
2019-11-11 00:21:40 -0800

    homelink in characterlist

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,25 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
+//
+// export default function CharacterList() {
+//   // TODO: Add useState to track data from useEffect
+//
+//   useEffect(() => {
+//     // TODO: Add API Request here - must run in `useEffect`
+//     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+//   }, []);
+//
+//   return (
+//     <section className="character-list">
+//       <h2>TODO: `array.map()` over your state here!</h2>
+//     </section>
+//   );
+// }
+import React from "react";
+import CharacterCard from "./CharacterCard";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
-
-  useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
-
-  return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
-  );
-}
+const HomeLink = styled.h3`
+    text-align: center;
+    padding: 20px;
+`;
