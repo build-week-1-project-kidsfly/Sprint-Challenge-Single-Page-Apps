11deb49b72d7d5c407ca085ba679eef12aad8113
josepaniagua <jpanjr@gmail.com>
2019-11-11 00:39:09 -0800

    episodelist wireframe

---
+++ src/components/EpisodeList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import EpisodeCard from "./EpisodeCard";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const HomeLink = styled.h3`
+    text-align: center;
+    padding: 20px;
+`;
+
+const EpisodeList = props => {
+    return (
+        <div></div>
+    );
+};
+
+export default EpisodeList;
