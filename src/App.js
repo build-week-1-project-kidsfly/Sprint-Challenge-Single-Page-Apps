75f5b37e22ecea18e5d4daefe6b5ffc7b9452ed3
josepaniagua <jpanjr@gmail.com>
2019-11-11 01:52:35 -0800

    axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import TabExampleBasic from "./components/TabNav";
+import CharacterList from "./components/CharacterList";
+import LocationList from "./components/LocationsList";
+import EpisodeList from "./components/EpisodeList";
 import Header from "./components/Header.js";
+import { Route } from "react-router-dom";
+import axios from "axios";
 
 
 export default function App() {
+    const [info, setInfo] = useState([]);
+    const [location, setLocation] = useState([]);
+    const [episode, setEpisode] = useState([]);
+
+    useEffect(() => {
+        axios.all([
+            axios.get('https://rickandmortyapi.com/api/character'),
+            axios.get('https://rickandmortyapi.com/api/location'),
+            axios.get('https://rickandmortyapi.com/api/episode')
+        ])
+            .then(axios.spread((cRes, lRes, eRes) =>{
+                setInfo(cRes.data.results)
+                setLocation(lRes.data.results)
+                setEpisode(eRes.data.results)
+            }))
+    },[])
+
   return (
     <main>
       <Header />
