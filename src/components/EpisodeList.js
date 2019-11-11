b26a2761311c30622bbc5ff75fb25586780ab2d5
josepaniagua <jpanjr@gmail.com>
2019-11-11 00:44:35 -0800

    finished episodelist

--- src/components/EpisodeList.js
+++ src/components/EpisodeList.js
@@ -9,8 +9,22 @@ const HomeLink = styled.h3`
 `;
 
 const EpisodeList = props => {
+    const { episode } = props;
     return (
-        <div></div>
+        <div>
+            <HomeLink>
+                <Link to="/">Home</Link>
+            </HomeLink>
+            <div className="cards">
+                {episode.map(item => {
+                    const { id, name, air_date, episode } = item
+                    return <EpisodeCard key={id} name={name} air_date={`Aired on ${air_date}`} episode={`Episode #: ${episode}`} />
+                })}
+            </div>
+            <HomeLink>
+                <Link to="/">Home</Link>
+            </HomeLink>
+        </div>
     );
 };
 
